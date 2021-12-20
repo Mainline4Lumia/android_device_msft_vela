2d4d954015e464a6f441fc42bbf984be5e527594
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-20 01:56:22 +0100

    vela: Initial commit
    Change-Id: I1b2802eca28e397fea53d0a3acfc9376ca398c04

---
+++ device.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Display
+TARGET_DISPLAY_ENABLE_DRM := true
+
+# Inherit from mainline-common
+$(call inherit-product, device/linux/mainline-common/mainline.mk)
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+# Include prebuilt kernel
+$(call inherit-product-if-exists, device/msft/vela-kernel/kernel.mk)
+
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.mainline \
+    fstab.mainline_ramdisk \
+    init.device.rc
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
